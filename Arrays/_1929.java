ea35179339bbae39b133ceac49411df89c8a9648
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-11-19 21:59:54 +0530

    Add files via upload

--- Arrays/_1929.java
+++ Arrays/_1929.java
@@ -1,1 +1,18 @@
-
+import java.util.Arrays;
+public class _1929{
+    public static void main(String[] args){
+        int arr[] = {1,3,2,1};
+        System.out.println(Arrays.toString(getConcatenation(arr)));
+    }
+    public static int[] getConcatenation(int arr[]) {
+        int n = arr.length;
+        int ans[] = new int[2 * n];
+        for(int i = 0; i < n; i++){
+            ans[i] = arr[i];
+        }
+        for(int i = 0; i < n; i++){
+            ans[i + n] = arr[i];
+        }
+        return ans;
+    }
+}
