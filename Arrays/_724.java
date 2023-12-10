72723218b376fa618b23853fc689c31ef41ad770
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-12-11 04:00:57 +0530

    Add files via upload

---
+++ Arrays/_724.java
@@ -0,0 +1,40 @@
+public class _1991{
+	public static void main(String[] args){
+		int arr[] = {2,1,-1	};
+		System.out.println(findMiddleIndex(arr));
+	}
+	public static int findMiddleIndex(int[] arr){
+        int n = arr.length;
+        if(n == 1){
+        	return 0;
+        }
+        int pref[] = new int[n];
+        int suff[] = new int[n];
+        createArray(pref, suff, arr);
+        if(suff[1] == 0){
+        	return 0;
+        }
+        for(int i = 1; i < n - 1; i++){
+        	int part1 = pref[i - 1];
+        	int part2 = suff[i + 1];
+        	if(part1 == part2){
+        		return i;
+        	}
+        }
+        if(pref[n - 2] == 0){
+        	return n - 1;
+        }
+        return -1;
+    }
+    public static void createArray(int pref[], int suff[], int arr[]){
+    	int n = arr.length, i = 0, j = n - 1, sum1 = 0, sum2 = 0;
+    	while(i < n){
+    		sum1 += arr[i];
+    		pref[i] = sum1;
+    		sum2 += arr[j];
+    		suff[j] = sum2;
+    		i++;
+    		j--;
+    	}
+    }
+}
