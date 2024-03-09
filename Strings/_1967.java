6aa9d82f59e4967673bd569e2e838a9ef11dc95a
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2024-03-09 16:06:34 +0530

    Add files via upload

--- Strings/_1967.java
+++ Strings/_1967.java
@@ -1,1 +1,19 @@
-S
+package Strings;
+
+public class _1967 {
+    public static void main(String[] args) {
+        String[] arr = {"a","a","a"};
+        String str = "ab";
+        System.out.println(numOfStrings(arr, str));
+    }
+
+    public static int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (String str : patterns) {
+            if (word.contains(str)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
