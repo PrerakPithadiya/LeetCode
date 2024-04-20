61a589a15c2385376afdc8bbc7ea84b194de89d1
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-04-20 18:19:40 +0530

    Update 2283.java

--- Strings/2283.java
+++ Strings/2283.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public boolean digitCount(String num) {
+        int[] freq = new int[10];
+        for (int i = 0; i < num.length(); i++) {
+            char ch = num.charAt(i);
+            freq[ch - 48]++;
+        }
+        for (int i = 0; i < num.length(); i++) {
+            int d1 = freq[i];
+            int d2 = num.charAt(i) - 48;
+            if (d1 != d2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
