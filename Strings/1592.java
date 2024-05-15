ce2c5c985f6e5b8b3e01664741d6f1ebff89298b
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-05-15 12:25:02 +0530

    Update 1592.java

--- Strings/1592.java
+++ Strings/1592.java
@@ -1,67 +1,59 @@
 class Solution {
     public String reorderSpaces(String str) {
         int n = str.length();
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
 
-		// count spaces
-		int spaces = 0;
-		for (int i = 0; i < n; i++) {
-			if (str.charAt(i) == ' ') {
-				spaces++;
-			}
-		}
+        // count spaces
+        int spaces = 0;
+        for (int i = 0; i < n; i++) {
+            if (str.charAt(i) == ' ') {
+                spaces++;
+            }
+        }
 
 
-		// count words -> " practice   makes   perfect"
-		List<String> list = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			char ch = str.charAt(i);
-			if (ch == ' ') {
-				if (sb.length() != 0) {
-					list.add(sb.toString());
-				    sb.delete(0, sb.length());
-				}
-			} else {
-				sb.append(ch);
-			}
-		}
-		if (sb.length() != 0) {
-			list.add(sb.toString());
-		    sb.delete(0, sb.length());
-		}
-		int words = list.size();
+        // count words
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') {
+                if (!sb.isEmpty()) {
+                    list.add(sb.toString());
+                    sb.delete(0, sb.length());
+                }
+            } else {
+                sb.append(ch);
+            }
+        }
+        if (!sb.isEmpty()) {
+            list.add(sb.toString());
+            sb.delete(0, sb.length());
+        }
+        int words = list.size();
 
-        
+
         if (spaces == 0 && words == 1) {
             return str;
         }
 
 
-		// calculating between spaces
-		StringBuilder spaceBet = new StringBuilder();
-		int sBetween = 0;
-        if (words == 1) {
-            sBetween = 0;
-        } else {
+        // calculating between spaces
+        StringBuilder spaceBet = new StringBuilder();
+        int sBetween = 0;
+        if (words != 1) {
             sBetween = spaces / (words - 1);
         }
-        for (int i = 1; i <= sBetween; i++) {
-			spaceBet.append(" ");
-		}
+        spaceBet.append(" ".repeat(Math.max(0, sBetween)));
 
 
-		// calculating end spaces
+        // calculating end spaces
         StringBuilder spaceEnd = new StringBuilder();
-        int sEnd = 0;
-        if (words == 1) {
-            sEnd = spaces;
-        } else {
+        int sEnd = spaces;
+        if (words != 1) {
             sEnd = spaces % (words - 1);
         }
-        for (int i = 1; i <= sEnd; i++) {
-            spaceEnd.append(" ");
-        }
+        spaceEnd.append(" ".repeat(Math.max(0, sEnd)));
 
 
         // calculating result (final string)
