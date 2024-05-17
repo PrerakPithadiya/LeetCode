602fb216fed4e01868be455e171d6aa06257c842
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-05-17 11:05:45 +0530

    Create 2469.java

---
+++ Strings/2469.java
@@ -0,0 +1,7 @@
+class Solution {
+    public double[] convertTemperature(double cel) {
+      double kel = cel + 273.15;
+		  double fah = cel * 1.80 + 32.00;
+		  return new double[]{kel, fah};
+    }
+}
