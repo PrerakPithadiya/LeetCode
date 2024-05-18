380b397c2f6c0a61404cce4d52cecc9d943a9585
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-05-19 00:39:31 +0530

    Create 1189.java

---
+++ Strings/1189.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxNumberOfBalloons(String s) {
+        int n = s.length(), count = 0;
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            freq[ch - 97]++;
+        }
+        while (true) {
+            if (freq[1] >= 1 && freq[0] >= 1 && freq[11] >= 2 && freq[14] >= 2 && freq[13] >= 1) {
+                freq[1]--;
+                freq[0]--;
+                freq[11] -= 2;
+                freq[14] -= 2;
+                freq[13]--;
+            } else {
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
